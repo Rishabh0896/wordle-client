47951dd816d4462eb890e53125b60ae89b3bce14
Rishabh0896 <gupta.risha@northeastern.edu>
2024-09-11 14:11:14 -0400

    Updating the logic to select the right port

--- src/arg_parser.py
+++ src/arg_parser.py
@@ -7,7 +7,8 @@ def parse_args(args=None):
     parser = argparse.ArgumentParser(description="Client program to connect to a server.")
 
     # Add optional arguments
-    parser.add_argument('-p', type=int, help="tcp port the server is listening on", default=Config.get_port())
+    # If the port argument is not provided assume that the port is a non TLS port
+    parser.add_argument('-p', type=int, help="tcp port the server is listening on", default=Config.DEFAULT_PORT)
     parser.add_argument('-s', action='store_true', help="use TLS encrypted socket connection")
 
     # Add required positional arguments
@@ -15,4 +16,13 @@ def parse_args(args=None):
     parser.add_argument('username', type=str, help="northeastern username")
 
     # Parse the arguments
-    return parser.parse_args(args)
+    args = parser.parse_args(args)
+
+    # Logic for choosing ports based on TLS flag
+    if args.s:
+        # If TLS is enabled and no port is specified, use TLS port
+        if args.p == Config.DEFAULT_PORT:
+            args.p = Config.TLS_PORT
+
+
+
